f3dce28258e561da7f178e37291793e32b5a6281
tobysoby <tobias.doll@dw.com>
2016-12-23 13:20:01 +0100

    Change port also in app.js

--- import_config.rb
+++ import_config.rb
@@ -10,6 +10,16 @@ def import_config
 		end
 		if config_line.index("port")
 			@port = config_line.sub("port=", "")
+			change_port_in_angular(@port)
 		end
 	end
+end
+
+def change_port_in_angular (new_port)
+	file_name = "./public/assets/js/app.js"
+	text = File.read(file_name)
+	new_contents = text.gsub(/localhost:\d{4}/, "localhost:" + new_port)
+
+	# To write changes to the file, use:
+	File.open(file_name, "w") {|file| file.puts new_contents }
 end
